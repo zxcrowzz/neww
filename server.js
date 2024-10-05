66fa7dbfe47ac719eabd8147b709f63fa010adfc
zxcrowzz <pantsbro4@gmail.com>
2024-10-05 10:56:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const { ExpressPeerServer } = require('peer');
-const { v4: uuidV4 } = require('uuid');
+const { v4: uuidV4 } = require("uuid");
 
 const app = express();
 const server = http.createServer(app);
@@ -34,4 +34,7 @@ io.on('connection', socket => {
     });
 });
 
-server.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
